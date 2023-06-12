c98c860f811db92f3030e2c063ffeba11363af8c
Daniel La Vista <daniel.j.lavista@gmail.com>
2023-06-12 15:17:16 +0930

    Messing around with variables

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Variables;
+
+class Program {
+    static void Main(string[] args) {
+        var number = 2;
+        var count = 10;
+        var totalPrice = 20.95f;
+        var character = 'A';
+        var firstName = "Daniel";
+        var isWorking = true;
+
+        Console.WriteLine(number);
+        Console.WriteLine(count);
+        System.Console.WriteLine(totalPrice);
+        System.Console.WriteLine(character);
+        System.Console.WriteLine(firstName);
+        System.Console.WriteLine(isWorking);
+
+        System.Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+
+        const float Pi = 3.14f;
+    }
+}
