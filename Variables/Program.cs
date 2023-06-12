5ff1799bfa7d13b691a4ccced46f1642fb03ea55
Daniel La Vista <daniel.j.lavista@gmail.com>
2023-06-12 17:51:24 +0930

    Messing around with type conversions

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -20,5 +20,33 @@ class Program {
         System.Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
 
         const float Pi = 3.14f;
+
+        //Type conversion
+        byte b = 1;
+        int i = b;
+
+        System.Console.WriteLine(i);
+
+        int i2 = 1000;
+        byte b2 = (byte)i2;
+
+        System.Console.WriteLine(b2);
+        try {
+            var number2 = "1234";
+            byte b3 = Convert.ToByte(number2);
+
+            System.Console.WriteLine(b3);
+        }
+        catch (Exception) {
+            System.Console.WriteLine("Number could not be converted to a byte");
+        }
+        try {
+            string str = "true";
+            bool b4 = Convert.ToBoolean(str);
+            System.Console.WriteLine(b4);
+        }
+        catch (Exception) {
+            System.Console.WriteLine("String could not be converted to a boolean");
+        }
     }
 }
